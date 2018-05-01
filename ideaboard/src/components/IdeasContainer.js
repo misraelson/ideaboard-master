b1926039926b1f086a41b7e49022550ab85505ee
misraelson <misraelson@gmail.com>
2018-05-01 12:31:23 -0700

    add and display saved message

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -9,7 +9,8 @@ class IdeasContainer extends Component {
     super(props)
     this.state = {
       ideas: [],
-      editingIdeaId: null
+      editingIdeaId: null,
+      notification: ""
     }
   }
 
@@ -31,10 +32,10 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
-  updateIdea = (idea)=> {
+  updateIdea = (idea) => {
     const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
     const ideas = update(this.state.ideas, {[ideaIndex]: { $set: idea }})
-    this.setState({ideas: ideas})
+    this.setState({ideas: ideas, notification: "All changes saved!"})
   }
 
   render() {
@@ -44,6 +45,9 @@ class IdeasContainer extends Component {
           <button className="newIdeaButton" onClick={this.addNewIdea} >
             New Idea
           </button>
+          <span className="notification">
+            {this.state.notification}
+          </span>
         </div>
         {this.state.ideas.map((idea) => {
           if(this.state.editingIdeaId === idea.id) {
